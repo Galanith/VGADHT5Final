5259528348a108ddee6b48f99cc987e4c9faadb3
Galanith <Austin.dodd@Triosstudent.com>
2018-04-03 16:15:43 -0400

    Stars completed in the game screen.

--- scripts/game-screen.js
+++ scripts/game-screen.js
@@ -17,17 +17,49 @@ kogeki.screens["game-screen"] = (function() {
 		canvas.width = rect.width;
 		canvas.height = rect.height;
 		
+		for(i = 0; i < generateRandom(45, 60); i++) {
+			stars.push(generateStar());
+		}
+		
 		playArea.appendChild(createBackground());
 		playArea.appendChild(canvas);
+		requestAnimationFrame(updateAll);
 	}
 	
 	function setup() {
 		
 	}
 
+	function updateAll() {
+		for(var i = 0; i < stars.length; i++) {
+			star = stars[i];
+			//console.log("Time started: " + star.timeStart);
+			//console.log("Time Elapsed: " + Date.now() - star.timeStart + "  Time total: " + star.timeTotal);
+			if(Date.now() - star.timeStart >= star.timeTotal) {
+				stars.splice(i, 1);
+				stars.push(generateStar());
+			}
+		}
+		ctx.fillStyle = "rgb(0, 0, 0)";
+		ctx.fillRect(0, 0, rect.width, rect.height);
+		ctx.fillStyle = "rgb(250, 255, 200)";
+		stars.forEach(function(e) {
+			if(e.changeCount >= e.changeTime) {
+				e.changeCount = 0;
+				e.xSize = Math.floor(generateRandom(1, 3));
+				e.ySize = Math.floor(generateRandom(1, 3));
+			} else {
+				e.changeCount++;
+			}
+			ctx.fillRect(e.x, e.y, e.xSize, e.ySize);
+		});
+		requestAnimationFrame(updateAll);
+	}
+	
 	function createBackground() {
 		var background = document.createElement("canvas"),
-			ctx = background.getContext('2d');
+			ctx = background.getContext('2d'),
+			star;
 			
 		kogeki.dom.addClass(background, "background");
 		background.width = rect.width;
@@ -35,17 +67,34 @@ kogeki.screens["game-screen"] = (function() {
 		ctx.fillStyle = "rgb(0, 0, 0)";
 		ctx.fillRect(0, 0, rect.width, rect.height);
 		ctx.fillStyle = "rgb(250, 255, 200)";
-		for(i = 0; i < generateRandom(20, 50); i++) {
-			console.log("Memes!");
-			var x = generateRandom(0, rect.width),
-				y = generateRandom(0, rect.height);
-			ctx.fillRect(x, y, 1, 1);
-		}
+		stars.forEach(function(e) {
+			ctx.fillRect(e.x, e.y, 1, 1);
+		});
 		return background;
 	}
 
 	function generateRandom(min, max){
 		return Math.random() * (max - min) + min;
+	}
+	
+	function generateStar() {
+		var x = generateRandom(0, rect.width),
+				y = generateRandom(0, rect.height),
+				timeStart, timeTotal;
+				
+			timeStart = Date.now();
+			timeTotal = generateRandom(2, 6) * 1000;
+			star = {
+				timeStart: timeStart,
+				timeTotal: timeTotal,
+				x: x,
+				y: y,
+				changeCount: 0,
+				changeTime: generateRandom(2, 15),
+				xSize: 1,
+				ySize: 1
+			}
+		return star;
 	}
 	
 	function run() {
